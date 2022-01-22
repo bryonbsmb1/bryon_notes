2044f1cb044aef4ed615b1c4059d744a2610641b
bryonbsmb1 <87393401+bryonbsmb1@users.noreply.github.com>
2022-01-21 23:06:11 -0500

    app.get added to index.js

--- server.js
+++ server.js
@@ -17,3 +17,23 @@ const PORT =process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
+app.use(express.static('public'));
+
+
+    app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+        });
+    app.get('/notes', (req, res) => {
+      res.sendFile(path.join(__dirname, '/public/notes.html'));
+        });
+    app.get('/api/notes', (req, res) => {
+      fs.readFile('./db/db.json', (err, data) => {
+          if (err) throw err;
+          res.send(JSON.parse(data));  
+        });
+        });
+
+
+
+
+        
